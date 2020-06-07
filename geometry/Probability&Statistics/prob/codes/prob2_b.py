cd9da1cc755cd325de96a530ccba51e583e44d51
SiddharthPh <51734363+SiddharthPh@users.noreply.github.com>
2020-06-07 18:41:48 +0530

    Add files via upload

--- geometry/Probability&Statistics/prob/codes/prob2_b.py
+++ geometry/Probability&Statistics/prob/codes/prob2_b.py
@@ -1,17 +1,17 @@
 import numpy as np
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import bernoulli
-sample_size=6
-event_size=3.14/4
+sample_size=144
+event_size=20
 prob=event_size/sample_size
+
+
 data_bern = bernoulli.rvs(size=1000,p=prob)
 ax = sns.distplot(data_bern,
-	                 kde=True,
-	                 bins=int(10),
+					bins=int(10),
+	                 kde=True,	               
 	                color='crimson',
-                  hist_kws={"linewidth": 20,'alpha':1})
-ax.set(xlabel='Bernouli', ylabel='Frequency')
-
+                  hist_kws={"linewidth": 25,'alpha':1})
+ax.set(xlabel='Bernouli', ylabel='P_Density')                 
 plt.show()
