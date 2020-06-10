b28e3419505802b6db73908eab097887c29216ea
SiddharthPh <51734363+SiddharthPh@users.noreply.github.com>
2020-06-11 01:32:31 +0530

    Add files via upload

--- geometry/Linear Algebra/codes/triangle/shaded.py
+++ geometry/Linear Algebra/codes/triangle/shaded.py
@@ -18,10 +18,29 @@ import shlex
 n1 = np.array([1,-1])
 n2 = np.array([3,2])
 n3 = np.array([0,1])
+c =  np.array([-1,12])
 
 c1 = -1
 c2 = 12
 c3 = 0
+#Intercept 1
+e1 = np.array([1,0]) 
+e2 = np.array([0,1]) 
+A1= c1*e1/(n1@e1)
+B1 = c1*e2/(n1@e2)
+k1 = 0
+k2 = 3
+N=np.vstack((n1,n2))
+D1=np.linalg.inv(N)@c
+x_A1B1 =  line_dir_pt(n1,D1,k1,k2)
+plt.plot(x_A1B1[0,:],x_A1B1[1,:],label='$line1$')
+
+#Intercept2
+A2 = c2*e1/(n2@e1)
+B2 = c2*e2/(n2@e2)
+x_A2B2 =   line_dir_pt(n2,D1,k1,k2)
+plt.plot(x_A2B2[0,:],x_A2B2[1,:],label='$line2$')
+
 
 #Intersection of the lines
 A=line_intersect(n1,c1,n2,c2)
@@ -31,7 +50,7 @@ C=line_intersect(n3,c3,n1,c1)
 points = np.array((A,B,C))
 
 #Filling up the desired region
-plt.fill(points[:,0], points[:,1], alpha=1)
+plt.fill(points[:,0], points[:,1], alpha=0.5)
 
 #Plotting points of Intersection
 plt.plot(A[0], A[1], 'o')
@@ -46,11 +65,6 @@ plt.ylabel('$y$')
 plt.legend(loc='best')
 plt.grid() # minor
 
-#if using termux
-#plt.savefig('./line/figs/line_ineq_mult.pdf')
-#plt.savefig('./line/figs/line_ineq_mult.eps')
-#subprocess.run(shlex.split("termux-open ./line/figs/line_ineq_mult.pdf"))
-#else
 plt.show()
 
 
